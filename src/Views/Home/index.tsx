e2e7893dddbc6265d87a11f7df57c0bac4f400c5
unknown <elituffley@gmail.com>
2022-03-24 14:04:46 +1300

    fixing prefetch

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,10 +1,18 @@
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Layout = dynamic(() => import("../Components/Layout"));
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.prefetch("/about");
+    router.prefetch("/store");
+  }, []);
+
   return (
     <Layout>
       <Head>
