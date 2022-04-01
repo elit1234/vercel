ee4249b83ebc93e27e9c955320be2eb29b6c54b0
unknown <elituffley@gmail.com>
2022-04-01 16:13:01 +1300

    bug fixes

--- src/Views/ViewingItem/index.tsx
+++ src/Views/ViewingItem/index.tsx
@@ -244,14 +244,31 @@ const ViewingItem = ({ id }: any) => {
           if (!showOptions) clickedShowOptions();
         }}
       >
-        {!showOptions && <h2>Options</h2>}
+        {!showOptions && (
+          <div className="viewingItem-option">
+            Options
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />
+              <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z" />
+            </svg>
+          </div>
+        )}
         {showOptions && (
           <>
             <div
               className="viewingItem-closeIcon"
               onClick={() => clickedHideOptions()}
             >
-              X
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+              </svg>
             </div>
             {options &&
               options.map((opt, key) => {
