e8279bf9a8583a982cf9c1a2e7b94cf4e24c63a5
unknown <elituffley@gmail.com>
2022-04-08 12:54:19 +1200

    docker compose fixes

--- pages/api/lib/redis.ts
+++ pages/api/lib/redis.ts
@@ -2,6 +2,7 @@ import Redis from "ioredis";
 
 const redis = new Redis(`redis://${process.env.REDIS_URL}`, {
   lazyConnect: true,
+  password: process.env.REDIS_PASSWORD,
 });
 
 export default redis;
